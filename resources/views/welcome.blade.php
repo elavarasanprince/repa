649c732901065842607ec17c727b852e8cc25eb3
elavarasanprince <elavarasan.p@techovirish.com>
2025-04-02 12:34:43 +0530

    banner change

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -83,7 +83,7 @@
                                 <div class="hero-content">
                                     <h6 data-animation="slideInRight" data-duration="2s" data-delay=".3s">WELCOME TO REPA </h6>
                                     <h1 data-animation="slideInRight" data-duration="2s" data-delay=".5s">
-                                        Need for REPA
+                                        Need for REPAa
 
                                     </h1>
                                     <p data-animation="slideInRight" data-duration="2s" data-delay=".7s">
